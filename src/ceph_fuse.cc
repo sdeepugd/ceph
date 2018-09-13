4f7b02d793a6248a9ef85b6ecdf01e36a90ea748
deepak.srinivasan <deepak.srinivasan@zohocorp.com>
2018-09-13 23:45:30 +0530

    debug

--- src/ceph_fuse.cc
+++ src/ceph_fuse.cc
@@ -228,8 +228,8 @@ int main(int argc, const char **argv, const char *envp[]) {
     ofstream lfile = logger->getLoggerFile();
     MonClient *mc = new MonClient(g_ceph_context);
     int r = mc->build_initial_monmap();
-    lfile<<"hello";
-    mc->monmap.print_summary(lfile);
+    lfile<<"hello"
+    mc->monmap.print(lfile);
     if (r == -EINVAL) {
       cerr << "failed to generate initial mon list" << std::endl;
       exit(1);
